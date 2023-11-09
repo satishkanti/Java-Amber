426b93ad0879c0055919a9a8c00272caecad2272
Satish Kanti <satishkanti22@gmail.com>
2023-11-09 19:18:52 +0530

    class person

---
+++ Day 7/Tight coupling/Person.java
@@ -0,0 +1,9 @@
+public class Person {
+    
+    public static void main(String[] args) {
+        Travel t1 = new Bus();
+        Travel t2 = new Train();
+        t1.start();
+        t2.start();
+    }
+}
