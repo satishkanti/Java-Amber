e72ed8bb0bbdebebee281edc91e6f395e2b9f72d
Satish Kanti <satishkanti22@gmail.com>
2023-11-10 18:20:15 +0530

    class calculateSquare implementing square

---
+++ Day 8/CalculateSquare.java
@@ -0,0 +1,9 @@
+public class CalculateSquare implements Square{
+    
+    public static void main(String[] args) {
+        Square c1 = new CalculateSquare();
+        c1.square(4);
+            
+        
+    }
+}
