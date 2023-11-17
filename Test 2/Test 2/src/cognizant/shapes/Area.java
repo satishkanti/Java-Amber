6cca2d324b6007a4ce4b37f049ffffe22eba7894
Satish Kanti <satishkanti22@gmail.com>
2023-11-17 12:43:11 +0530

    test2

---
+++ Test 2/Test 2/src/cognizant/shapes/Area.java
@@ -0,0 +1,9 @@
+package cognizant.shapes;
+
+public class Area {
+    
+    public static void main(String[] args) {
+        Circle c1 = new Circle(2.5f);
+        System.out.println("Radius from constructor 2: " + c1.getRadius());
+    }
+}
