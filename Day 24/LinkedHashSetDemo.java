e935a0f33dc280822032c15b8be0749a2eb16df5
Satish Kanti <satishkanti22@gmail.com>
2023-12-12 12:19:07 +0530

    Linked hash set

---
+++ Day 24/LinkedHashSetDemo.java
@@ -0,0 +1,13 @@
+import java.util.LinkedHashSet;
+
+public class LinkedHashSetDemo {
+
+    public static void main(String[] args){
+
+        LinkedHashSet<String> l1 = new LinkedHashSet<String>();
+        l1.add("HTML");
+        l1.add("CSS");
+        l1.add("Java");
+        System.out.println(l1);
+    }
+}
